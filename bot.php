5f0dc5bca3549698416317c14352a8782300a7da
jirattapan <31032166+jirattapan@users.noreply.github.com>
2018-01-18 12:42:28 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -18,14 +18,10 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-        {
+        
 				  'type' => 'text',
           'text' => "เปิดไฟแล้ว"
-        },
-        {
-          'type' => 'text',
-          'text' => "ปิดไฟแล้ว"
-        }
+        
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
